a72660beab6b1805a481fbdeae13efd7c25fde98
muzammil <muzammil.azam88@gmail.com>
2021-05-06 15:04:13 +0500

    get meta coin address from 2 ways

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,5 +4,9 @@ const MetaCoin = artifacts.require("MetaCoin");
 module.exports = async (deployer) => {
   await deployer.deploy(ConvertLib);
   await deployer.link(ConvertLib, MetaCoin);
-  await deployer.deploy(MetaCoin);
+  const metaCoinInstance = await deployer.deploy(MetaCoin);
+
+  // You can use both . cute choice is yours
+  console.log("metaCoinInstance", metaCoinInstance.address);
+  console.log("MetaCoin.address", MetaCoin.address);
 };
